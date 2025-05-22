c22726e4683912822d779c99d37940f4d767314c
Mihir Raj Chowdhury <rajmihir945@gmail.com>
2025-05-22 23:07:31 +0530

    changed the use of openAI to Gemini

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain import PromptTemplate
-from langchain_openai import OpenAI
+from langchain_google_genai import GoogleGenerativeAI
 
-
+# Prompt template
 template = """
     Below is a draft text that may be poorly worded.
     Your goal is to:
@@ -11,21 +11,18 @@ template = """
     - Convert the draft text to a specified dialect
 
     Here are some examples different Tones:
-    - Formal: Greetings! OpenAI has announced that Sam Altman is rejoining the company as its Chief Executive Officer. After a period of five days of conversations, discussions, and deliberations, the decision to bring back Altman, who had been previously dismissed, has been made. We are delighted to welcome Sam back to OpenAI.
-    - Informal: Hey everyone, it's been a wild week! We've got some exciting news to share - Sam Altman is back at OpenAI, taking up the role of chief executive. After a bunch of intense talks, debates, and convincing, Altman is making his triumphant return to the AI startup he co-founded.  
+    - Formal: Greetings! OpenAI has announced that Sam Altman is rejoining...
+    - Informal: Hey everyone, it's been a wild week...
 
     Here are some examples of words in different dialects:
-    - American: French Fries, cotton candy, apartment, garbage, \
-        cookie, green thumb, parking lot, pants, windshield
-    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, \
-        car park, trousers, windscreen
+    - American: French Fries, cotton candy, apartment...
+    - British: chips, candyfloss, flat...
 
     Example Sentences from each dialect:
-    - American: Greetings! OpenAI has announced that Sam Altman is rejoining the company as its Chief Executive Officer. After a period of five days of conversations, discussions, and deliberations, the decision to bring back Altman, who had been previously dismissed, has been made. We are delighted to welcome Sam back to OpenAI.
-    - British: On Wednesday, OpenAI, the esteemed artificial intelligence start-up, announced that Sam Altman would be returning as its Chief Executive Officer. This decisive move follows five days of deliberation, discourse and persuasion, after Altman's abrupt departure from the company which he had co-established.
+    - American: Greetings! OpenAI has announced that Sam Altman is rejoining...
+    - British: On Wednesday, OpenAI, the esteemed artificial intelligence start-up...
 
-    Please start the redaction with a warm introduction. Add the introduction \
-        if you need to.
+    Please start the redaction with a warm introduction. Add the introduction if you need to.
     
     Below is the draft text, tone, and dialect:
     DRAFT: {draft}
@@ -35,90 +32,70 @@ template = """
     YOUR {dialect} RESPONSE:
 """
 
-#PromptTemplate variables definition
+
 prompt = PromptTemplate(
-    input_variables=["tone", "dialect", "draft"],
-    template=template,
+    input_variables = ["tone","draft","dialect"],
+    template = template
 )
 
-
-#LLM and key loading function
-def load_LLM(openai_api_key):
-    """Logic for loading the chain you want to use should go here."""
-    # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
+def load_LLM(google_api_key):
+    import os
+    # google_api_key = os.environ["GOOGLE_API_KEY"]
+    llm = GoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7, google_api_key=google_api_key)
     return llm
 
 
-#Page title and header
 st.set_page_config(page_title="Re-write your text")
-st.header("Re-write your text")
+st.header("Welcome, What do you want me to rewrite?")
 
-
-#Intro: instructions
-col1, col2 = st.columns(2)
+col1,col2=st.columns(2)
 
 with col1:
     st.markdown("Re-write your text in different styles.")
-
 with col2:
-    st.write("Contact with [AI Accelera](https://aiaccelera.com) to build your AI Projects")
+    st.write("Contact with [Mihir Raj Chowdhury](Mihirrajchowdhury.netlify.app) to build your AI Projects")
 
-
-#Input OpenAI API Key
-st.markdown("## Enter Your OpenAI API Key")
-
-def get_openai_api_key():
-    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input", type="password")
+st.markdown("## Enter Your Gemini API Key")
+def get_google_api_key():
+    input_text = st.text_input(
+        label="Gemini API Key", 
+        placeholder="Ex: AIzaSyD4-abc123...", 
+        key="google_api_key_input", 
+        type="password"
+    )
     return input_text
 
-openai_api_key = get_openai_api_key()
+google_api_key = get_google_api_key()
 
-
-# Input
 st.markdown("## Enter the text you want to re-write")
-
 def get_draft():
     draft_text = st.text_area(label="Text", label_visibility='collapsed', placeholder="Your Text...", key="draft_input")
     return draft_text
 
 draft_input = get_draft()
 
-if len(draft_input.split(" ")) > 700:
-    st.write("Please enter a shorter text. The maximum length is 700 words.")
-    st.stop()
-
-# Prompt template tunning options
 col1, col2 = st.columns(2)
 with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your redaction to have?',
-        ('Formal', 'Informal'))
-    
+    option_tone = st.selectbox('Which tone would you like your redaction to have?', ('Formal', 'Informal', 'Geeky','Bold'))
 with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
-    
-    
-# Output
+    option_dialect = st.selectbox('Which English Dialect would you like?', ('American', 'British', 'Indian'))
+
 st.markdown("### Your Re-written text:")
 
+
 if draft_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. \
-            Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', 
-            icon="⚠️")
+    if not google_api_key:
+        st.warning('Please insert Gemini API Key. Instructions [here](https://makersuite.google.com/app/apikey)', icon="⚠️")
         st.stop()
 
-    llm = load_LLM(openai_api_key=openai_api_key)
-
+    llm = load_LLM(google_api_key=google_api_key)
+    
     prompt_with_draft = prompt.format(
         tone=option_tone, 
         dialect=option_dialect, 
         draft=draft_input
     )
 
-    improved_redaction = llm(prompt_with_draft)
+    improved_redaction = llm.invoke(prompt_with_draft)
 
     st.write(improved_redaction)
